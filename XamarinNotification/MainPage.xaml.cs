562cf09b3e18b6b23c2e9dc74441e11137b551c4
DelpinSusaiRaj <susairajs@outlook.com>
2017-02-27 09:50:11 +0530

    Add all files

---
+++ XamarinNotification/MainPage.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+
+namespace XamarinNotification
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_OnClicked(object sender, EventArgs e)
+        {
+            DisplayAlert("Notification", "Do you Love Xamarin ?", "Yes", "No");
+        }
+    }
+}
